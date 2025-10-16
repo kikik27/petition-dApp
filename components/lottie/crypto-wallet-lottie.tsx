ba203573e3075f86f7451508293413082a6774c6
zaky <mkikik27@gmail.com>
2025-10-16 15:37:26 +0700

    feat: update CryptoWalletLottie component to include a wrapper div and adjust animation size

--- components/lottie/crypto-wallet-lottie.tsx
+++ components/lottie/crypto-wallet-lottie.tsx
@@ -3,6 +3,10 @@
 import animation from "@/public/Crypto-Wallet.json";
 import Lottie from "lottie-react";
 
-const CryptoWalletLottie = () => <Lottie width={40} height={40} animationData={animation} loop={true} />;
+const CryptoWalletLottie = () => (<>
+<div className="max-w-52">
+<Lottie width={30} height={30} animationData={animation} loop={true} />
+</div>
+</>);
 
 export default CryptoWalletLottie;
